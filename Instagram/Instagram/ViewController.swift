d920b0d50262e214ef9b48ff987d64cc77307cab
coraysong <coraysong1991@msn.cn>
2019-08-28 16:16:04 +0800

    项目初始化

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 宋超 on 2019/8/28.
+//  Copyright © 2019 宋超. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
